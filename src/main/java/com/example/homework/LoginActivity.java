a5c0ede734a05b36dee1f1b17bbc82f8c6ce7a92
sooa <166204173+HamSooah@users.noreply.github.com>
2024-06-17 05:06:21 +0900

    Add files via upload

---
+++ src/main/java/com/example/homework/LoginActivity.java
@@ -0,0 +1,91 @@
+package com.example.homework;
+
+import static com.example.homework.SignupActivity.SEP;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText mEditId;
+    EditText mEditPw;
+    Button login_Button;
+    Button signup_Button;
+    Button product_Button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setTitle("로그인");
+
+        mEditId = (EditText) findViewById(R.id.login_id_input);
+        mEditPw = (EditText) findViewById(R.id.login_pw_input);
+
+        SharedPreferences prefs = getSharedPreferences("person_info", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        SharedPreferences login_prefs = getSharedPreferences("login_info", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor login_editor = login_prefs.edit();
+
+        // 최근 로그인한 아이디 자동 표시
+        String recentId = login_prefs.getString("recentId", "");
+        mEditId.setText(recentId);
+
+        // 로그인 버튼
+        login_Button = (Button) findViewById(R.id.login_Button);
+        // 로그인 버튼 클릭시, 상품 페이지로 이동
+        login_Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (mEditId.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "아이디를 입력하세요", Toast.LENGTH_SHORT).show();
+                } else {
+                    String prefsUser = prefs.getString(mEditId.getText().toString(), "");
+                    String[] saveData = prefsUser.split(SEP);
+                    if(saveData[0].equals(mEditId.getText().toString())) {
+                        if (saveData[1].equals(mEditPw.getText().toString())) {
+                            login_editor.putBoolean("isLogin", true);
+                            login_editor.putString("loginId", mEditId.getText().toString());
+                            login_editor.putString("recentId", mEditId.getText().toString());
+                            login_editor.apply();
+                            Intent intent = new Intent(getApplicationContext(), ProductActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "비밀번호가 틀렸습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "아이디를 확인하세요", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        //회원가입 버튼
+        signup_Button = (Button) findViewById(R.id.signup_Button);
+        //회원가입 버튼 클릭시, 회원가입 페이지로 이동
+        signup_Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 메인으로 버튼
+        product_Button = (Button) findViewById(R.id.product_Button);
+        //메인 버튼 클릭시, 상품 페이지로 이동
+        product_Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                login_editor.putBoolean("isLogin", false);
+                login_editor.remove("loginId");
+                login_editor.apply();
+                Intent intent = new Intent(getApplicationContext(), ProductActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ src/main/java/com/example/homework/SignupActivity.java
@@ -0,0 +1,110 @@
+package com.example.homework;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SignupActivity extends AppCompatActivity {
+    private EditText mEditId;
+    private Button mDupButton;
+    private EditText mEditPw;
+    private EditText mEditName;
+    private EditText mEditPhone;
+    private EditText mEditAddress;
+    private RadioButton mAgreeDecline;
+    private RadioButton mAgreeAccept;
+    private boolean isIdDup = false;
+    private Button signup_commit;
+
+    // 유효성 검사 함수
+    boolean check_validation(String password) {
+        // 비밀번호 유효성 검사식 : 숫자, 특수문자가 포함되어야 한다.
+        String val_symbol = "([0-9].*[!,@,#,^,&,*,(,)])|([!,@,#,^,&,*,(,)].*[0-9])";
+        // 정규표현식 컴파일
+        Pattern pattern_symbol = Pattern.compile(val_symbol);
+        Matcher matcher_symbol = pattern_symbol.matcher(password);
+        if (matcher_symbol.find()) {
+            return true;
+        }else {
+            return false;
+        }
+    };
+
+    // Preferences에 회원 정보 회원 정보를 담은 긴 문자열로 저장
+    static final String SEP = "@#!~";
+    void addMember(String id, String pw, String name, String phone, String address, Boolean isAgree, SharedPreferences.Editor editor) {
+        String key = id;
+        String value = id + SEP + pw + SEP + name + SEP + phone + SEP + address + SEP +isAgree;
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        setTitle("회원가입");
+
+        mEditId = (EditText) findViewById(R.id.signup_id_input);
+        mEditPw = (EditText) findViewById(R.id.signup_pw_input);
+        mEditName = (EditText) findViewById(R.id.signup_name_input);
+        mEditPhone = (EditText) findViewById(R.id.signup_phone_input);
+        mEditAddress = (EditText) findViewById(R.id.signup_address_input);
+        mAgreeDecline = (RadioButton) findViewById(R.id.signup_agree_decline);
+        mAgreeAccept = (RadioButton) findViewById(R.id.signup_agree_accept);
+
+        SharedPreferences prefs = getSharedPreferences("person_info", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        // 아이디 중복 확인 버튼
+        mDupButton = (Button) findViewById(R.id.signup_id_isDup);
+        mDupButton.setOnClickListener(v -> {
+            String prefsUser = prefs.getString(mEditId.getText().toString(), "");
+            String[] saveData = prefsUser.split(SEP);
+            if (mEditId.getText().toString().isEmpty()) {
+                Toast.makeText(getApplicationContext(), "아이디를 입력해주세요", Toast.LENGTH_SHORT).show();
+                isIdDup = false;
+            } else if(saveData[0].equals(mEditId.getText().toString())) {
+                Toast.makeText(getApplicationContext(), "이미 사용 중인 아이디입니다.", Toast.LENGTH_SHORT).show();
+                isIdDup = false;
+            } else {
+                Toast.makeText(getApplicationContext(), "사용 가능한 아이디입니다.", Toast.LENGTH_SHORT).show();
+                isIdDup = true;
+            }
+        });
+
+        // 회원가입 완료 버튼
+        signup_commit = (Button) findViewById(R.id.signup_commit);
+        // 회원가입 완료 버튼 클릭시, 로그인 페이지로 이동
+        signup_commit.setOnClickListener(v -> {
+            if (isIdDup) {
+                if (mEditPw.getText().toString().length() >= 4 && mEditPw.getText().toString().length() < 9) {
+                    if (check_validation(mEditPw.getText().toString())) {
+                        if (mAgreeAccept.isChecked()) {
+                                addMember(mEditId.getText().toString(), mEditPw.getText().toString(), mEditName.getText().toString(),
+                                        mEditPhone.getText().toString(), mEditAddress.getText().toString(), true, editor);
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "수집 동의하셔야 회원가입 가능합니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "숫자와 특수문자가 포함되어야 합니다.", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "비밀번호는 4~8글자만 가능합니다.", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "아이디 변경한 뒤 중복 확인 후 다시 시도하세요", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+};
